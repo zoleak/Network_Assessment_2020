4345d77aa8d2915d038ae6f6a71cb198d80121bf
Zolea <Kevin.Zolea@dep.nj.gov>
2019-12-03 12:16:17 -0500

    changes to labels

--- NJ_population_density_map-2.R
+++ NJ_population_density_map-2.R
@@ -1,14 +1,14 @@
-### Script to create map of population density for all Municipality in NJ with 
-### ambient air quality monitoring stations. This script will utlize the tidycensus package to 
-### retrieve population data from the US Census Bureau ###
-### Author: Kevin Zolea; Date: 9/2019 ###
+### Script to create map of population density for all Municipalities in NJ with 
+### ambient air quality monitoring stations ###
+### Author: Kevin Zolea; Date: 12/2019 ###
 ########## Download necessary packages to make script run ##################################
 if (!require(pacman)) {
   install.packages('pacman')
   
 }
 
-pacman::p_load("tidyverse","tidycensus","sf","ggsn","cowplot","viridis","readxl")
+pacman::p_load("tidyverse","tidycensus","sf","ggsn","cowplot","viridis","readxl","ggrepel")
+pacman::p_load_gh("ggsflabel")
 ###########################################################################################
 ### Install package from github that allows nice labeling on maps ###
 #devtools::install_github("yutannihilation/ggsflabel")
@@ -28,6 +28,7 @@ NJ_air_stations<-st_as_sf(NJ_air_stations,coords = c("LON", "LAT"))
 NJ_air_stations <- st_set_crs(NJ_air_stations, "+proj=longlat +datum=WGS84")
 ### Read in NJ muncipalities shapefile ###
 NJ_muni<-st_read(dsn= getwd(),layer="New_Jersey_Municipalities")
+### Need to make changes to values in dataframe so we can join the shapefile and data together to map ###
 NJ_pop_estimates$MUN<-gsub("TOWNSHIP","TWP",NJ_pop_estimates$MUN)
 NJ_pop_estimates$MUN<-gsub("BOROUGH","BORO",NJ_pop_estimates$MUN)
 NJ_pop_estimates$MUN<-gsub("PEAPACK AND GLADSTONE BORO","PEAPACK-GLADSTONE BORO",NJ_pop_estimates$MUN)
@@ -36,6 +37,8 @@ NJ_pop_estimates$MUN<-gsub("HILLSBORO TWP","HILLSBOROUGH TWP",NJ_pop_estimates$M
 nj_pop_muni<-left_join(NJ_pop_estimates,NJ_muni,by = "MUN")
 nj_pop_muni<-st_as_sf(nj_pop_muni)
 ###########################################################################################
+### THIS IS JUST TO LOOK BACK ON THIS WILL NOT BE USED ###
+##########################################################
 ### Create connection to API with special API key from US Census Bureau ###
 #census_api_key("1c32b297e99b22d82fb12c683f56d95eb9e12168")
 ###########################################################################################
@@ -44,6 +47,7 @@ nj_pop_muni<-st_as_sf(nj_pop_muni)
 #                                variables = "DENSITY",geometry = T)
 ###########################################################################################
 ### Make breaks for legend ###
+### This is needed to make the different color breaks in the legend ###
 nj_pop_muni$brks <- cut(nj_pop_muni$pop_density_2018, 
                    breaks=c(0, 10, 25, 50, 100, 250,500,1000,2500,5000,58000), 
                    labels=c("0 - 10", "10 - 25", "25 - 50",
@@ -55,16 +59,16 @@ pop_map<-ggplot()+
   geom_sf(data = nj_pop_muni,aes(fill=brks))+
   geom_sf(data = NJ_air_stations,aes(color="NJ Ambient Air Quality Monitoring Stations"),
           size=3.5,key_glyph = draw_key_dotplot)+
-  geom_sf_text(data=NJ_air_stations,aes(label = SITE_NAME),nudge_x = 3.5)+
-  #geom_sf_label_repel(data = NJ_air_stations,aes(label=SITE_NAME))+
   #geom_sf_text_repel(data = subset(NJ_air_stations, X < -75),aes(label = SITE_NAME),
   #                    force = 100, nudge_x = 3.5- subset(NJ_air_stations, X < -75)$X,hjust=1,segment.size = 0.2,
   #                    direction     = "y",
   #                    hjust         = 0)+
-  #geom_sf_label_repel(data = subset(NJ_air_stations, X > -75),aes(label = SITE_NAME),
+  #geom_sf_text_repel(data = subset(NJ_air_stations, X > -75),aes(label = SITE_NAME),
   #                    force = 100, nudge_x = 2.7- subset(NJ_air_stations, X > -75)$X,hjust=1,segment.size = 0.2,
   #                    direction     = "y",
   #                    hjust         = 1)+
+  geom_sf_text_repel(data = NJ_air_stations,aes(label = SITE_NAME),
+                                         nudge_x = 2.7,fontface="bold")+
   ggtitle("New Jersey Population Density")+
   xlab("")+
   ylab("")+
@@ -88,8 +92,12 @@ pop_map<-ggplot()+
                                   "#FFAE42","orange","#FF4500","red","#8b0000"))+
   scale_color_manual("",
                      values = c("NJ Ambient Air Quality Monitoring Stations"="black"))
-### Creates north arrow for map using the ggsn package ###
+
+                         
+### Creates north arrow for map using the ggsn package and saves map ###
+pdf(file="NJ_pop_density_2018_estimates.pdf",width = 13,height = 11)
 north2(pop_map, x = 0.35, y = 0.15, scale = 0.1, symbol = 3)
+dev.off()
 ###########################################################################################
 
 
